687c6312b7cd11f93d163b27c3b0d20112707aa0
Unknown <clint.ferrin@gmail.com>
2017-04-26 22:00:43 -0600

    Updated Script

--- setup_linux.sh
+++ setup_linux.sh
@@ -4,28 +4,21 @@
 ####################
 
 # check to see if vim folder exists
-if [ ! -d "$~/.vim" ]; then
-    mkdir "~/.vim"  
-fi
+mkdir ~/.vim  
 
 # back up old vimrc
-if [ -f "$~/.vimrc" ]; then
-    mv ~/.vimrc ~/.vimrc_old
-fi
+mv ~/.vimrc ~/.vimrc_old
 
 # move new vim files to home 
 mv vim/.vimrc ~/
-mv vim/* ~/.vim/
-
+mv vim/ ~/.vim/
 
 ####################
 ##    tmux setup  ##
 ####################
 
 # check if tmux conf file exists
-if [ -f "$~/.tmux.conf" ]; then
-    mv ~/.tmux.conf ~/.tmux.conf_old
-fi
+mv ~/.tmux.conf ~/.tmux.conf_old
 
 # move new tmux.conf file to home
 mv tmux/.tmux.conf ~/.tmux.conf
